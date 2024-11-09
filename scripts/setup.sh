5b98bfd729401d3a0267b0853afb112817ea7316
fredgido <fredgido@gmail.com>
2024-11-09 17:52:12 +0000

    make example config for docker development

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
+echo "hi"
 npm i
+echo "npm i"
 dfx identity new codespace_dev --storage-mode=plaintext
 dfx identity use codespace_dev
+echo "identity crap done"
 dfx start --background
+echo "dfxx start background done"
 dfx stop
+echo "dfxx stop background done"
 npm i -g ic-mops
+echo "npm"
 mops install
+echo "mops install"
+
+sleep infinity
